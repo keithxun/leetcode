e058742fb76604afecb325f0fe829cfa9d9a1d2c
Yan Xin Chew <chewyanxin@gmail.com>
2026-03-05 09:42:50 +0800

    O(n) java

---
+++ 1758. Minimum Changes to Make Alternating Binary String/yanxin.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(String s) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0 && s.charAt(i) == '1') {
+                count++;
+            } else if (i % 2 == 1 && s.charAt(i) == '0') {
+                count++;
+            }
+        }
+
+        return Math.min(count, s.length() - count);
+    }
+}
